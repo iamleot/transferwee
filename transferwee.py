53ca997e7384ef84fd14193ef34814a441bbebe0
Peter Krantz <peter.krantz@gmail.com>
2023-05-16 17:42:32 +0200

    Update transferwee.py. Fixes #40
    Depending on the origin the parsed csrf token may not be needed. This fix updates the session only if there was a csrf token meta element in the html.

--- transferwee.py
+++ transferwee.py
@@ -187,16 +187,18 @@ def _prepare_session() -> Optional[requests.Session]:
     requests.
     """
     s = requests.Session()
-    s.headers.update({'User-Agent': WETRANSFER_USER_AGENT})
+    s.headers.update({
+        'User-Agent': WETRANSFER_USER_AGENT,
+        'x-requested-with': 'XMLHttpRequest'
+    })
     r = s.get('https://wetransfer.com/')
     m = re.search('name="csrf-token" content="([^"]+)"', r.text)
-    if not m:
-        logger.error(f'Could not find any csrf-token')
-        return None
-    s.headers.update({
-        'x-csrf-token': m.group(1),
-        'x-requested-with': 'XMLHttpRequest',
-    })
+    if m:
+        s.headers.update({
+            'x-csrf-token': m.group(1),
+        })
+    else:
+        logger.info('Could not find any csrf-token')
 
     return s
 
